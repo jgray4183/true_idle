7841931c6b70eea79c281aad39e07fe40de12bd9
jgray4183 <j.gray.4183@gmail.com>
2024-11-10 22:27:08 +0000

    forgor how to commit?

---
+++ main.py
@@ -0,0 +1,26 @@
+>>> while true: 
+...     def main():
+...     	points = 0
+... 	genirators = 0
+... 	
+...     def genirate(p, g):
+...             new_points = p + g
+...             return new_points
+... 
+...     def buy_genirators(p, g):
+...             new_points = p - 10
+...             new_genirators = g + 1
+...             return new_points, new_genirators
+... 	
+...     if points > 10:
+...             buy_genirators (f{points}, {genirators})
+...             print ("Genirator bought")
+...             else genirate(f{points}, {genirators})
+...                     break
+... 		
+...     print(f"New Points {genirate (f{points}, {genirators})})
+... 
+... 
+...     time.sleep(1)
+... 
+...     main()
