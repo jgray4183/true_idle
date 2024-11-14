0affb9732602876db22b170f7f0ff0a98a890578
jgray4183 <j.gray.4183@gmail.com>
2024-11-14 02:39:31 +0000

    began to move to OOP

---
+++ objects.py
@@ -0,0 +1,8 @@
+object Genirators:
+    def __init__(self):
+        self.cost = 10
+        self.genirate = 2
+        self.mutipler = 1
+        
+    def genirate(self):
+        return self.genirate ** self.mutipler
