a3a16e7d284a3e72d1cac2a84e240a8715634287
jgray4183 <j.gray.4183@gmail.com>
2024-11-28 19:23:07 +0000

    Added pristege and more upgrades

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from constants import BASE_PRICE
 import time
 
 points = 0 
+max_pristege = 5
 running = True
 gen_list =  [Generator(1)]
-upgrade_dict = {"buy_amount" : Upgrade("Buy Amount", 25, 3)}
+upgrade_dict = {"buy_amount" : Upgrade("Buy Amount", 25, 3, 100), "tickspeed" : Upgrade("Tickspeed", 100, 100, 25), "unlock_bank" : UpgradeStoreValue("Unlock Bank", 250, 5, 100, 50)}
 
 def new_generator(points):
-    points_local = points
+    points_local = points + upgrade_dict["unlock_bank"].value
     new_gen_number = len(gen_list) + 1
     gen_list.append(Generator(new_gen_number))
-    points_local -= gen_list[-1].price
+    points_cost = gen_list[-1].price - upgrade_dict["unlock_bank"].value
+    if gen_list[-1].price > upgrade_dict["unlock_bank"].value:
+        upgrade_dict["unlock_bank"].value = 0
+        return points_local - points_cost
+    upgrade_dict["unlock_bank"].value -= gen_list[-1].price
     return points_local
 
 def main():
@@ -23,17 +28,25 @@ def main():
         #generate points for each generator
         for gen in gen_list:
             points += gen.generate()
+        points -= upgrade_dict["unlock_bank"].add_value(int((points/50)//1))
         points_generated = points - points_start
         points_start = points
-        #first unlock new generator if possible
-        if points >= BASE_PRICE ** (len(gen_list) + 1):
-            points = new_generator(points)
-        #increase buy ammount if possible
-        for upgrade in upgrade_dict:
-            if points >= upgrade_dict[upgrade].price:
-                upgrade_dict[upgrade].buy(points)
-        #buy 1 of each generator that can be afforded starting at cheapest
+        #pristege a generator if possible
         for gen in gen_list:
+                if gen.amount > gen.pristege_cost and gen.pristege < max_pristege:
+                        gen_list.append(PristegedGenerator(gen))
+                        gen_list.remove(gen)
+                        gen_list.sort(key=lambda e:e.gen_val)
+        #unlock new generator if possible
+        if points + upgrade_dict["unlock_bank"].value >= BASE_PRICE ** (len(gen_list) + 1):
+            points = new_generator(points)
+            gen_list.sort(key=lambda e:e.gen_val)
+        #buy upgrades as they are fewer and more impactful than gens
+        for upgrade in upgrade_dict:
+            if points >= upgrade_dict[upgrade].price and upgrade_dict[upgrade].tier < upgrade_dict[upgrade].max:
+                upgrade_dict[upgrade].buy(points)
+        #buy buy max of each generator that can be afforded starting at higest genirating
+        for gen in reversed(gen_list):
             if points >= gen.price:
                 buy_amount = int(points // gen.price)
                 if upgrade_dict["buy_amount"].tier < buy_amount:
@@ -47,17 +60,35 @@ def main():
 
 #bellow this line is all for the user output
 
+        total_gens = 0
+        for gen in gen_list:
+                total_gens += gen.amount
+
         highest_gen = gen_list[-1]
 
-        print ("============================================")
+        print ("============================================================")
         if len(gen_list) > gen_number_start:
             print (f"{highest_gen} unlocked")
-        if points_spent > 0:
-            print(f"{points_generated} points generated \n{points_spent} points spent \nYou have {highest_gen.amount} {highest_gen} \nNew Points {points}")
+        if highest_gen.amount == 1:
+                if points_spent > 0:
+                        print(f"{points_generated} points generated \n{points_spent} points spent \nYou have {total_gens} generators, {highest_gen.amount} is {highest_gen} \nNew Points {points}")
+                else:
+                        print(f"{points_generated} points generated \nYou have {total_gens} generators, {highest_gen.amount} is {highest_gen} \nNew Points {points}") 
         else:
-            print(f"{points_generated} points generated \nYou have {highest_gen.amount} {highest_gen}  \nNew Points {points}")
+                if points_spent > 0:
+                        print(f"{points_generated} points generated \n{points_spent} points spent \nYou have {total_gens} generators, {highest_gen.amount} are {highest_gen} \nNew Points {points}")
+                else:
+                        print(f"{points_generated} points generated \nYou have {total_gens} generators, {highest_gen.amount} are {highest_gen} \nNew Points {points}")
 
-        time.sleep(1)
+        if upgrade_dict["unlock_bank"].value > 0:
+                print (f"You have {upgrade_dict["unlock_bank"].value} points saved to unlock Tier {(len(gen_list) + 1)}")
+
+        #sleep bassed on tickspeed upgrade
+
+        if (0.01 * upgrade_dict["tickspeed"].tier) < 0.5:
+                time.sleep(1 - (0.01 * upgrade_dict["tickspeed"].tier))
+        else:
+                time.sleep (0.5)
 
 if __name__ == "__main__":
     main()
--- objects.py
+++ objects.py
@@ -4,6 +4,8 @@ class Generator():
     def __init__(self, tier):
         self.tier = tier
         self.amount = 1
+        self.pristege = 0
+        self.pristege_cost = 100
         self.gen_val = None
         self.price = None
         self.get_gen()
@@ -30,11 +32,24 @@ class Generator():
             points_output = points - (self.price)
             return points_output
 
+class PristegedGenerator(Generator):
+    def __init__(self, gen):
+        super().__init__(gen.tier)
+        self.gen_val = int((gen.gen_val * 1.5)//1)
+        self.price = int((gen.price * 1.25)//1)
+        self.pristege = gen.pristege + 1
+        self.pristege_cost = int((gen.pristege_cost * 1.5)//1)
+        print (f"Generator {self.tier} has reached Pristege {self.pristege}")
+
+    def __str__(self):
+        return f"Pristege {self.pristege} Generator {self.tier}"
+
 class Upgrade():
-    def __init__(self, name, price, multiplier):
+    def __init__(self, name, price, multiplier, maximum):
         self.name = name
         self.price = price
         self.multiplier = multiplier
+        self.max = maximum
         self.tier = 1
 
     def buy(self, points):
@@ -47,3 +62,30 @@ class Upgrade():
             self.price *= self.multiplier
             print (f"{self.name} Upgraded")
             return points_local
+
+class UpgradeStoreValue(Upgrade):
+    def __init__(self, name, price, multiplier, maximum, value_max):
+        super().__init__(name, price, multiplier, maximum)
+        self.value = 0
+        self.value_max = value_max
+
+    def add_value(self, amount):
+        if self.value + amount > self.value_max:
+            diferance = self.value
+            self.value = self.value_max
+            return self.value_max - diferance
+        else:
+            self.value += amount
+            return amount
+
+    def buy(self, points):
+        if self.price > points:
+            raise Exception("not enough points")
+            return points
+        else:
+            self.tier += 1
+            points_local = points - self.price
+            self.value_max *= self.value_max
+            self.price *= self.multiplier
+            print (f"{self.name} Upgraded")
+            return points_local    
